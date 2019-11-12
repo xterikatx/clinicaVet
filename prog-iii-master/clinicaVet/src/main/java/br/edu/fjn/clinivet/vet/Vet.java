045de1b423c8c970f9d26ec1be1a34cef876716c
l <kk@kk.lk>
2019-11-11 21:33:05 -0300

    Entidades Veterinario e Serviços Iniciada

---
+++ prog-iii-master/clinicaVet/src/main/java/br/edu/fjn/clinivet/vet/Vet.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fjn.clinivet.vet;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author aluno
+ */
+@Entity
+public class Vet {
+    
+    @Id
+    @Column(nullable = false)
+    private String CRM;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn
+    private Services services;
+       
+    @Column(nullable = false)
+    private String CPF;
+    
+    @Column(nullable = false)
+    private String password;
+ 
+    public String getCRM() {
+        return CRM;
+    }
+
+    public void setCRM(String CRM) {
+        this.CRM = CRM;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+}
