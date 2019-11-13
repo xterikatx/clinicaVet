cd1a1a7015cbbcea056a7b06d79eb661a90bfed2
Erika Lopes <201810099@aluno.fjn.edu.br>
2019-11-12 21:21:27 -0300

    Customer Controller adicionado

--- prog-iii-master/clinicaVet/src/main/java/br/edu/fjn/clinivet/controller/Customer/CustomerController.java
+++ prog-iii-master/clinicaVet/src/main/java/br/edu/fjn/clinivet/controller/Customer/CustomerController.java
@@ -5,10 +5,43 @@
  */
 package br.edu.fjn.clinivet.controller.Customer;
 
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.edu.fjn.clinivet.annotations.Private;
+import br.edu.fjn.clinivet.model.customer.Customer;
+import java.util.Arrays;
+import java.util.List;
+import javax.inject.Inject;
+
 /**
  *
  * @author erika
  */
+//Declarando que essa classe é um Controller
+@Controller
 public class CustomerController {
-    
+
+    //lembra do que leo falou? "Não sei".
+    @Inject
+    private Result result;
+
+    //declarando metodo Get("rota")
+    //metodo void que deve ter o mesmo nome do jsp
+    @Get("newCustomer")
+    public void formCustomer() {
+
+    }
+
+    //declarando metodo Post ("rota")
+    //metodo void que deve ter o mesmo nome do jsp
+    @Post("saveCustomer")
+    public void save(Customer customer) {
+        /*System.out.println("Customer " + customer.getAddress().getCity());
+        result.include("msgSuccess", "Feito! "
+                + customer.getName() + " salvo com sucesso!");
+        result.redirectTo(this).formView();*/
+    }
 }
